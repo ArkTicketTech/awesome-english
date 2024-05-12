2b2ac42c68bf77db7bc333dc59f7cc0726a22c56
wfnuser <wfnuser@hotmail.com>
2024-05-12 21:35:19 +0800

    add wechat bot for english groups

---
+++ wecom-bot/index.js
@@ -0,0 +1,70 @@
+require("dotenv").config();
+const axios = require("axios");
+
+const fs = require("fs");
+const path = require("path");
+
+const indexFile = path.join(__dirname, "index.txt");
+const webhookUrl = `https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=${process.env.WEBHOOK_KEY}`;
+
+const resources = [
+  "[【最牛保姆级听力】美国人在工作中最真实的聊天，你能听懂多少？](https://b23.tv/A0Oqf9q)",
+];
+
+function saveIndex(index) {
+  fs.writeFileSync(indexFile, index.toString());
+}
+
+function loadIndex() {
+  if (fs.existsSync(indexFile)) {
+    return parseInt(fs.readFileSync(indexFile, "utf8"));
+  }
+  return 0;
+}
+
+function getDateString() {
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const weekDay = ["周日", "周一", "周二", "周三", "周四", "周五", "周六"][
+    date.getDay()
+  ];
+
+  return `${month}.${day}/${weekDay}`;
+}
+
+// 发送消息的函数
+async function sendMessage() {
+  const currentIndex = loadIndex();
+  const resource = resources[currentIndex % resources.length];
+
+  const messageContent = `
+	⭐大家早上好呀
+	${getDateString()}
+	
+	✍🏻今日加餐视频：
+	《保姆级听力训练》
+	
+	用碎片化时间，系统化学习口语听力
+	
+	今日共习内容:
+	${resource}
+	`;
+  const message = {
+    msgtype: "markdown",
+    markdown: {
+      content: messageContent,
+    },
+  };
+
+  try {
+    const response = await axios.post(webhookUrl, message);
+    console.log("Message sent successfully:", response.data);
+    saveIndex(currentIndex + 1);
+  } catch (error) {
+    console.error("Failed to send message:", error);
+  }
+}
+
+// 调用函数
+sendMessage();
